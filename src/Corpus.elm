4e137abd7f19a2d5b26ec98ebec0a3ac3a192faf
Ben Millwood <thebenmachine+git@gmail.com>
2018-09-24 00:25:28 +0800

    don't need a separate example for then

--- src/Corpus.elm
+++ src/Corpus.elm
@@ -6,9 +6,7 @@ Sentences that don't contain parseable content are ignored.
 
 If you're happy and you know it, clap your hands.
 
-If you're happy and you know it then clap your hands.
-
-If you aren't happy and you know it, do nothing.
+If you aren't happy and you know it then do nothing.
 
 If I had a million dollars, I would buy you a green dress.
 
